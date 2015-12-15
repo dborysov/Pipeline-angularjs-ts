ff9cae5b141fe016d6781a1d81b00c1ae6396453
Dmytro Borisov <d.borisov@levi9.com>
2015-12-15 14:22:05 +0200

    Moving constants to config file

--- gulp/config.js
+++ gulp/config.js
@@ -1,61 +1,77 @@
 'use strict';
+const path = require('path');
 
 const baseDir = {
     dest: 'dist',
     src: 'app'
-},
-    folderNames = {
-        bower: 'bower_components',
-        outputCss: 'css',
-        outputLibs: 'libs',
-        partials: 'partials'
+};
+const folderNames = {
+    bower: 'bower_components',
+    outputCss: 'css',
+    outputLibs: 'libs',
+    partials: 'partials'
+};
+const fileNames = {
+    outputJs: 'all.js',
+    templatesJs: 'templates.js',
+    indexHtml: 'index.html'
+};
+const moduleNames = {
+    app: 'app'
+};
+const src = {
+    ts: {
+        customMainFiles: [
+            `./${baseDir.src}/main.ts`,
+            `./${baseDir.src}/{controllers,services}/${'*.ts'}`
+        ]
     },
-    fileNames = {
-        outputJs: 'all.js',
-        templatesJs: 'templates.js',
-        indexHtml: 'index.html'
+    js: {
+        libs: [
+            'angular/angular.min.js',
+            'angular-ui-router/release/angular-ui-router.min.js'
+        ],
+        testLibs: ['bower_components/angular-mocks/angular-mocks.js'],
+        testSpecs: ['test/spec/**/*.js']
     },
-    moduleNames = {
-        app: 'app'
+    css: {
+        libs: ['bootstrap/dist/css/bootstrap.min.css']
     },
-    src = {
-        ts: {
-            customMainFiles: [
-                `./${baseDir.src}/main.ts`,
-                `./${baseDir.src}/{controllers,services}/${'*.ts'}`
-            ]
-        },
-        js: {
-            libs: [
-                'angular/angular.min.js',
-                'angular-ui-router/release/angular-ui-router.min.js'
-            ],
-            testLibs: ['bower_components/angular-mocks/angular-mocks.js'],
-            testSpecs: ['test/spec/**/*.js']
-        },
-        css: {
-            libs: ['bootstrap/dist/css/bootstrap.min.css']
-        },
-        sass: {
-            custom: [`./${baseDir.src}/Content/Sass/+(*.sass|*.scss)`]
-        },
-        html: {
-            main: `./${baseDir.src}/index.html`,
-            partials: [`./${baseDir.src}/${folderNames.partials}/${'*.html'}`]
-        }
+    sass: {
+        custom: [`./${baseDir.src}/Content/Sass/+(*.sass|*.scss)`]
     },
-    taskConfigs = {
-        compileTemplates: {
-            minify: {
-                collapseWhitespace: true,
-                removeAttributeQuotes: true,
-                removeComments: true
-            },
-            moduleName: moduleNames.app,
-            output: fileNames.templatesJs,
-            strip: folderNames.partials
-        }
-    };
+    html: {
+        main: `./${baseDir.src}/index.html`,
+        partials: [`./${baseDir.src}/${folderNames.partials}/${'*.html'}`]
+    },
+    configs: {
+        karma: path.resolve('karma.conf.js')
+    },
+    get testFiles() {
+        const mainLibs = src.js.libs.map(lib => path.join(folderNames.bower, lib));
+        const customFiles = [fileNames.outputJs, fileNames.templatesJs].map(file => path.join(baseDir.dest, file));
+        const testLibs = src.js.testLibs;
+        const specs = src.js.testSpecs;
+        return [
+            ...mainLibs,
+            ...customFiles,
+            ...testLibs,
+            ...specs
+        ];
+    }
+};
+const taskConfigs = {
+    compileTemplates: {
+        minify: {
+            collapseWhitespace: true,
+            removeAttributeQuotes: true,
+            removeComments: true
+        },
+        moduleName: moduleNames.app,
+        output: fileNames.templatesJs,
+        strip: folderNames.partials
+    }
+};
 
 module.exports = {
     baseDir,
--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -2,25 +2,12 @@
 'use strict';
 
 const gulp = require('gulp');
-const path = require('path');
 const karma = require('karma');
 const config = require('../config');
 const karmaParseConfig = require('karma/lib/config').parseConfig;
 
 gulp.task('test', ['default'], cb => {
-    const configFilePath = path.resolve('karma.conf.js');
-    const mainLibs = config.src.js.libs.map(lib => path.join(config.folderNames.bower, lib));
-    const customFiles =[config.fileNames.outputJs, config.fileNames.templatesJs].map(file => path.join(config.baseDir.dest, file));
-    const testLibs = config.src.js.testLibs;
-    const specs = config.src.js.testSpecs;
-
-    const karmaConfig = karmaParseConfig(configFilePath, {files: [
-                ...mainLibs,
-                ...customFiles,
-                ...testLibs,
-                ...specs
-            ]
-        });
+    const karmaConfig = karmaParseConfig(config.src.configs.karma, { files: config.src.testFiles });
 
     const server = new karma.Server(karmaConfig, exitCode => {
         cb();
